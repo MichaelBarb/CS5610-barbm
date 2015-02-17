2dad7590669eeadcf44436245d83426583c7e2b5
Michael Barb <barb.m@husky.neu.edu>
2015-02-16 20:01:49 -0500

    Week 5 jQuery UI More

---
+++ myTests/Week05/jQuery3.html.js
@@ -0,0 +1,26 @@
+﻿$(function () {
+    $("button:first").button({
+        icons: {
+            primary: "ui-icon-locked"
+        },
+        text: false
+    })
+        .next().button({
+            icons: {
+                primary: "ui-icon-locked"
+            }
+        })
+        .next().button({
+            icons: {
+                primary: "ui-icon-gear",
+                secondary: "ui-icon-triangle-1-s"
+            }
+        })
+        .next().button({
+            icons: {
+                primary: "ui-icon-gear",
+                secondary: "ui-icon-triangle-1-s"
+            },
+            text: false
+        });
+});
