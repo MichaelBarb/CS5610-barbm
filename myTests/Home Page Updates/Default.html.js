e4c6fc592d4117b2d20ec37f48a28b8d0b6ae25a
Michael Barb <barb.m@husky.neu.edu>
2015-04-05 22:43:18 -0400

    Home De-Bootstrapped

--- myTests/Home Page Updates/Default.html.js
+++ myTests/Home Page Updates/Default.html.js
@@ -1,1 +1,15 @@
 ﻿window.addEventListener("hashchange", function () { scrollBy(0, -60); });
+
+$(document).ready(function(){
+    $('.your-class').slick({
+        setting-name: setting-value
+    });
+});
+
+$('.autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+
